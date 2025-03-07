16054fba5cfd120343d6c71300513be45a072009
Christina Gaitan <christina.gaitan@gmail.com>
2025-03-07 10:40:25 -0600

    Add Users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_04_161651) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_07_162326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,5 +18,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_04_161651) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
