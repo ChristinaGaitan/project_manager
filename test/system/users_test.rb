4bcbd81ef956c7a9fc588e41207100b347b5e0cd
Christina Gaitan <christina.gaitan@gmail.com>
2025-03-07 10:53:43 -0600

    fix user tests

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -7,17 +7,17 @@ class UsersTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit users_url
-    assert_selector "h1", text: "Users"
+    assert_selector "h1", text: "Listing users"
   end
 
   test "should create user" do
     visit users_url
-    click_on "New user"
+    click_on "New User"
 
-    fill_in "Email", with: @user.email
+    fill_in "Email", with: "test#{@user.email}"
     fill_in "First name", with: @user.first_name
     fill_in "Last name", with: @user.last_name
-    click_on "Create User"
+    click_on "Save"
 
     assert_text "User was successfully created"
     click_on "Back"
@@ -25,12 +25,12 @@ class UsersTest < ApplicationSystemTestCase
 
   test "should update User" do
     visit user_url(@user)
-    click_on "Edit this user", match: :first
+    click_on "Edit", match: :first
 
     fill_in "Email", with: @user.email
     fill_in "First name", with: @user.first_name
     fill_in "Last name", with: @user.last_name
-    click_on "Update User"
+    click_on "Save"
 
     assert_text "User was successfully updated"
     click_on "Back"
@@ -38,8 +38,10 @@ class UsersTest < ApplicationSystemTestCase
 
   test "should destroy User" do
     visit user_url(@user)
-    click_on "Destroy this user", match: :first
 
+    accept_alert do
+      click_on "Destroy", match: :first
+    end
     assert_text "User was successfully destroyed"
   end
 end
